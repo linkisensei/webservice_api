020515a84f78f1ecc32dbde28cc4a67f6c3cca0d
Your Name <lucas.barreto@leolearning.com>
2025-03-23 20:56:35 -0300

    Adding params to api_route_helper

--- classes/helpers/routing/api_route_helper.php
+++ classes/helpers/routing/api_route_helper.php
@@ -13,11 +13,12 @@ class api_route_helper {
      * Generates an absolute API URI from a relative path.
      *
      * @param string $relative The relative path (e.g., 'users/1').
+     * @param array $params
      * @return string The absolute API URI.
      */
-    public static function get_api_absolute_uri(string $relative = ''): string {
+    public static function get_api_absolute_uri(string $relative = '', array $params = []): string {
         $relative = trim($relative, '/');
-        $url = new moodle_url("/webservice/api/{$relative}");
+        $url = new moodle_url("/webservice/api/{$relative}", $params);
         return $url->out(false);
     }
 
@@ -25,10 +26,11 @@ class api_route_helper {
      * Extracts the relative API URI from an absolute URL.
      *
      * @param string $absolute The absolute URL (e.g., 'https://example.com/webservice/api/users/1').
+     * @param array $params
      * @return string The relative API URI (e.g., '/users/1').
      */
-    public static function get_api_relative_uri(string $absolute): string {
-        $root = new moodle_url('/webservice/api');
+    public static function get_api_relative_uri(string $absolute, array $params = []): string {
+        $root = new moodle_url('/webservice/api', $params);
         return '/' . ltrim(substr($absolute, strlen($root->get_path())), '/');
     }
 
